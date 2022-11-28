ee218a9a5c177130ce9dadbbe4425d61183564b2
J-Cundiff <jd.cundiff@outlook.com>
2022-11-28 14:25:47 -0600

    Started Personal Site MVC Conversion

---
+++ PersonalSiteMVC/wwwroot/js/custom.js
@@ -0,0 +1,65 @@
+
+// GOOGLE MAP
+var map = '';
+var center;
+
+function initialize() {
+    var mapOptions = {
+      zoom: 16,
+      center: new google.maps.LatLng(13.758468, 100.567481),
+      scrollwheel: false
+    };
+  
+    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+
+    google.maps.event.addDomListener(map, 'idle', function() {
+        calculateCenter();
+    });
+  
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.setCenter(center);
+    });
+}
+
+function calculateCenter() {
+  center = map.getCenter();
+}
+
+function loadGoogleMap(){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    document.body.appendChild(script);
+}
+
+$(function(){
+  loadGoogleMap();
+});
+
+// NIVO LIGHTBOX
+$('#portfolio a').nivoLightbox({
+        effect: 'fadeScale',
+    });
+
+// HIDE MOBILE MENU AFTER CLIKING ON A LINK
+   $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+    //Let's select another node
+
+    let copyright = document.getElementById('copyright');
+
+    console.log(copyright);
+    let today = new Date();
+    console.log(today);
+    today = today.getFullYear();
+    console.log(today);
+    copyright.textContent = '&copy;';
+    copyright.innerHTML = '&copy;';
+    copyright.innerHTML = `&copy; ${today} Jeffrey Cundiff.`; 
+
+    $(function(){
+      var coverflow = $("#coverflow").flipster();
+    });
