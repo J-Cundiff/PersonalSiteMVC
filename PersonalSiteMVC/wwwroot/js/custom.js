b081e94797917d30a9e3507a923816b74f6ae1ee
J-Cundiff <jd.cundiff@outlook.com>
2022-12-08 12:42:02 -0600

    Added jQuery revealUp for animation

--- PersonalSiteMVC/wwwroot/js/custom.js
+++ PersonalSiteMVC/wwwroot/js/custom.js
@@ -63,3 +63,46 @@ $('#portfolio a').nivoLightbox({
     $(function(){
       var coverflow = $("#coverflow").flipster();
     });
+
+
+gsap.registerPlugin(ScrollTrigger);
+// REVEAL //
+gsap.utils.toArray(".revealUp").forEach(function (elem) {
+    ScrollTrigger.create({
+        trigger: elem,
+        start: "top 90%",
+        end: "bottom 20%",
+        onEnter: function () {
+            gsap.fromTo(
+                elem,
+                { y: 100, autoAlpha: 0 },
+                {
+                    duration: 1.25,
+                    y: 0,
+                    autoAlpha: 1,
+                    ease: "back",
+                    overwrite: "auto"
+                }
+            );
+        },
+        onLeave: function () {
+            gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+        },
+        onEnterBack: function () {
+            gsap.fromTo(
+                elem,
+                { y: -100, autoAlpha: 0 },
+                {
+                    duration: 1.25,
+                    y: 0,
+                    autoAlpha: 1,
+                    ease: "back",
+                    overwrite: "auto"
+                }
+            );
+        },
+        onLeaveBack: function () {
+            gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+        }
+    });
+});
